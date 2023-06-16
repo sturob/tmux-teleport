5c13eb8ceefd8556ea7dadabdceda841cfcd68b3
Stu Rob <git@sturob.com>
2023-06-16 06:15:25 -0400

    refocus current window when pasting it to a different session

--- scripts/window-paste.sh
+++ scripts/window-paste.sh
@@ -12,9 +12,12 @@ src_id="@$src_id_raw"
 target_index=$(tmux display-message -p -t "$target_id" -F '#{window_index}')
 target_session=$(tmux display-message -p -t "$target_id" -F '#{session_name}')
 
-
 current_id=$(tmux display-message -p -F '#{window_id}')
-# last_window_id=$(tmux list-windows -F "#{window_id} #{window_last_flag}" | grep '1$' | cut -d' ' -f1)
 this_session=$(tmux display-message -p '#{session_name}')
 
 $CURRENT_DIR/window-move.sh $src_id_raw $target_index $target_session
+
+if [[ "$src_id" == "$current_id" && "$this_session" != "$target_session" ]]; then # we're moving current cross-session
+	tmux select-window -t "$current_id"
+	tmux switch-client -t "$target_session"
+fi
