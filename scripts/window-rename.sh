f917d476a63ebb5c1ea83fd4006c64db3907ba1f
Stu Rob <git@sturob.com>
2023-06-16 22:21:25 -0400

    remove unneeded sleep

--- scripts/window-rename.sh
+++ scripts/window-rename.sh
@@ -5,5 +5,3 @@ window_name=$(tmux display-message -p -t "@$window_id" '#{window_name}')
 
 tmux command-prompt -I"$window_name" "rename-window -t \"@$window_id\" -- '%%'"
 
-sleep 1 # command-prompt blocks in 3.3 but attempt a better workaround 
-
