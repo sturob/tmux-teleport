4324db66bbea37fc2987e9db006321f08c75283a
Stu Rob <git@sturob.com>
2023-06-17 16:23:18 -0400

    don't prepopulate window name when renaming

--- scripts/window-rename.sh
+++ scripts/window-rename.sh
@@ -3,5 +3,5 @@
 window_id=$(echo $1 | awk '{print $1}')
 window_name=$(tmux display-message -p -t "@$window_id" '#{window_name}')
 
-tmux command-prompt -I"$window_name" "rename-window -t \"@$window_id\" -- '%%'"
+tmux command-prompt -p"rename from $window_name:" "rename-window -t \"@$window_id\" -- '%%'"
 
